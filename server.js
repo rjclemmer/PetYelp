0cdd9e34a4ecaed7c07d35b0ce2eefb4d307e97e
jannorth <116915245+jannorth@users.noreply.github.com>
2023-03-20 14:12:10 -0400

    commenting out code to prep for refactor

--- server.js
+++ server.js
@@ -36,25 +36,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
-// // route to display dynamic src images
-// app.get('/public', (req,res) => {
-//   imageList = []
-
-//   imageList.push({src: '/public/images/01-pug-wearing-glasses.jpg', name: 'Herbie'})
-//   imageList.push({src: '/public/images/02-grumpy-cat.jpg', name: 'Chipper'})
-//   imageList.push({src: '/public/images/03-hairless-cat.jpg', name: 'Chicken'})
-//   imageList.push({src: '/public/images/04-cozy-cat-in-blanket.jpg', name: 'Mittens'})
-//   imageList.push({src: '/public/images/05-backpack-cat.jpg', name: 'Cozmo'})
-//   imageList.push({src: '/public/images/06-long-haired-chihuahua.jpg', name: 'Shredder'})
-//   imageList.push({src: '/public/images/07-smiley-cat.jpg', name: 'Frank'})
-//   imageList.push({src: '/public/images/08-expressive-pug.jpg', name: 'Snickers'})
-//   imageList.push({src: '/public/images/09-side-eye-frenchie.jpg', name: 'Violette'})
-//   imageList.push({src: '/public/images/10-running-weimaraner.jpg', name: 'Charlotte'})
-
-//   res.render('main', { imageList: imageList });
-
-// });
-
 app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
